c6cc7608681fd49cfc1e7e1ecbd55626de64a2c5
Nikita Sergienko <nsergienko@aquivalabs.com>
2016-09-26 18:56:19 +0300

    testing queries

--- src/main/kotlin/com/gainsight/tasks/CaseCounter.kt
+++ src/main/kotlin/com/gainsight/tasks/CaseCounter.kt
@@ -43,7 +43,7 @@ open class CaseCounter @Autowired constructor(val dataSource: BasicDataSource) {
         if (doProcess) {
             val ids = idToCountMap.keys.joinToString ("&&, $$", "($$", "$$)", -1, "...")
             println("ids: $ids")
-            val usageQuery = "SELECT * FROM salesforce.JBCXM__UsageData__c WHERE sfid IN $ids"
+            val usageQuery = "SELECT * FROM salesforce.JBCXM__UsageData__c WHERE JBCXM__Account__c IN $ids"
             println(usageQuery)
             val usageStatement = conn.prepareStatement(usageQuery)
             val usageResult = usageStatement.executeQuery()
