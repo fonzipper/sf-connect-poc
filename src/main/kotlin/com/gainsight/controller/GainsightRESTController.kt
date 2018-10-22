99fe661306a2c638cd50f0053b76db739eb158e6
Nikita Sergienko <nsergienko@aquivalabs.com>
2018-10-22 14:17:04 +0200

    Removed some staff

--- src/main/kotlin/com/gainsight/controller/GainsightRESTController.kt
+++ src/main/kotlin/com/gainsight/controller/GainsightRESTController.kt
@@ -1,6 +1,5 @@
 package com.gainsight.controller
 
-import com.gainsight.data.PartnerConfig
 import com.gainsight.intefaces.PartnerConfigRepository
 import org.apache.commons.dbcp2.BasicDataSource
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,51 +17,51 @@ class GainsightRESTController @Autowired constructor(
 ) {
     @RequestMapping(value = ["/"], method = [RequestMethod.GET])
     fun getMainPage(): String{
-        val conn = dataSource.connection
-        val stmt = conn.prepareStatement("SELECT * FROM salesforce.workflow__c LIMIT 5")
-        val rs = stmt.executeQuery()
+//        val conn = dataSource.connection
+//        val stmt = conn.prepareStatement("SELECT * FROM salesforce.workflow__c LIMIT 5")
+//        val rs = stmt.executeQuery()
+//
+//        var res = ""
+//        while (rs.next()){
+//            res += rs.getString("name") + ','
+//            println(rs.getString("sfid"))
+//        }
 
-        var res = ""
-        while (rs.next()){
-            res += rs.getString("name") + ','
-            println(rs.getString("sfid"))
-        }
-
-        return "Hello there $res"
+        return "Hello there"
     }
 
     @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET])
     fun getWf(@PathVariable("id") id: String): String{
-        println("this is id: $id")
-        val conn = dataSource.connection
-        val stmt = conn.prepareStatement("SELECT * FROM salesforce.Workflow__c WHERE sfid LIKE \'%$id%\'")
-        val rs = stmt.executeQuery()
+//        println("this is id: $id")
+//        val conn = dataSource.connection
+//        val stmt = conn.prepareStatement("SELECT * FROM salesforce.Workflow__c WHERE sfid LIKE \'%$id%\'")
+//        val rs = stmt.executeQuery()
+//
+//        var res = ""
+//        while (rs.next()){
+//            res += rs.getString("name") + ' ' + rs.getString("workflow__c")
+//            println(rs.getString("sfid"))
+//        }
 
-        var res = ""
-        while (rs.next()){
-            res += rs.getString("name") + ' ' + rs.getString("workflow__c")
-            println(rs.getString("sfid"))
-        }
-
-        return "Selected workflow $res"
+        return "Selected workflow"
     }
 
     @RequestMapping(value = ["/partner"], method = [RequestMethod.GET])
     fun createPartner(
             @RequestParam("name", required = false) name: String?,
             @RequestParam("url", required = false) url: String?): String {
-        if (name == null) return "Parameters has not been provided"
-        var partner = repo.getByName(name)
-        if (url == null) {
-            return partner.toString() + "<br/>" + partner?.endpoint
-        }
-        if (partner == null) {
-            partner = PartnerConfig(name, url)
-        } else {
-            partner.endpoint = url
-        }
-        repo.save(partner)
+//        if (name == null) return "Parameters has not been provided"
+//        var partner = repo.getByName(name)
+//        if (url == null) {
+//            return partner.toString() + "<br/>" + partner?.endpoint
+//        }
+//        if (partner == null) {
+//            partner = PartnerConfig(name, url)
+//        } else {
+//            partner.endpoint = url
+//        }
+//        repo.save(partner)
 
-        return partner.toString() + "<br/>" + partner.endpoint
+        return "Oh, hi"
     }
 }
